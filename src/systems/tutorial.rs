c10b3c26679e8f150827b103bea37c42caabe3e7
mescgit <34687684+mescgit@users.noreply.github.com>
2025-06-12 19:06:22 -0500

    Display tutorial steps in UI

--- src/systems/tutorial.rs
+++ src/systems/tutorial.rs
@@ -2,13 +2,55 @@ use bevy::prelude::*;
 
 use crate::resources::tutorial::TutorialState;
 
+#[derive(Resource)]
+pub struct TutorialUi {
+    pub container: Entity,
+    pub text: Entity,
+}
+
 pub struct TutorialPlugin;
 
 impl Plugin for TutorialPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<TutorialState>()
+        app
+            .init_resource::<TutorialState>()
+            .add_systems(Startup, setup_tutorial_ui)
             .add_systems(Update, tutorial_system);
     }
+}
+
+fn setup_tutorial_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let container = commands
+        .spawn(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                left: Val::Px(20.0),
+                bottom: Val::Px(20.0),
+                padding: UiRect::all(Val::Px(8.0)),
+                ..default()
+            },
+            background_color: Color::rgba(0.0, 0.0, 0.0, 0.7).into(),
+            visibility: Visibility::Hidden,
+            ..default()
+        })
+        .id();
+
+    let text_entity = commands
+        .spawn(TextBundle::from_section(
+            "",
+            TextStyle {
+                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                font_size: 20.0,
+                color: Color::WHITE,
+            },
+        ))
+        .id();
+
+    commands.entity(container).push_children(&[text_entity]);
+    commands.insert_resource(TutorialUi {
+        container,
+        text: text_entity,
+    });
 }
 
 pub fn tutorial_system(world: &mut World) {
@@ -36,6 +78,20 @@ pub fn tutorial_system(world: &mut World) {
             title,
             content
         );
+
+        // Update on-screen tutorial text
+        let (container, text_entity) = {
+            let ui = world.resource::<TutorialUi>();
+            (ui.container, ui.text)
+        };
+
+        if let Some(mut text) = world.entity_mut(text_entity).get_mut::<Text>() {
+            text.sections[0].value = format!("{}\n{}", title, content);
+        }
+        world
+            .entity_mut(container)
+            .insert(Visibility::Visible);
+
         if let Some(action_fn) = action {
             action_fn(world);
         }
